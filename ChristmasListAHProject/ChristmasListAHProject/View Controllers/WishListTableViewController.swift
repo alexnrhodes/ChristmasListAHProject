f64e868901feeac5330d1a2758e9078f7e61b717
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-11-14 12:06:30 -0600

    Fix - Cell Identifier

--- ChristmasListAHProject/ChristmasListAHProject/View Controllers/WishListTableViewController.swift
+++ ChristmasListAHProject/ChristmasListAHProject/View Controllers/WishListTableViewController.swift
@@ -11,40 +11,42 @@ import UIKit
 class WishListTableViewController: UITableViewController {
     
     let santasWorkshop = SantasWorkshop()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    
+        
+        
     }
-
+    
     // MARK: - Table view data source
-
-   
-
+    
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
+        
         return santasWorkshop.giftBag.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "GiftCell", for: indexPath)
-
+        
         let gift = santasWorkshop.giftBag[indexPath.row]
         
         cell.textLabel?.text = gift.giftName
         cell.detailTextLabel?.text = "Category: \(gift.category)"
-
+        
         return cell
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "" {
-            guard let destinationVC = segue.destination as? AddGiftViewController, let indexPath = tableView.indexPathForSelectedRow else {return}
+        if segue.identifier == "AddGiftSegue" {
+            guard let destinationVC = segue.destination as? AddGiftViewController else {return}
             destinationVC.santasWorshop = self.santasWorkshop
+        } else if segue.identifier == "GiftDetailSegue" {
+            guard let destinationVC = segue.destination as? GiftDetailViewController, let indexPath = tableView.indexPathForSelectedRow else {return}
+            destinationVC.santasWorkshop = self.santasWorkshop
             destinationVC.gift = santasWorkshop.giftBag[indexPath.row]
         }
     }
-
 }
